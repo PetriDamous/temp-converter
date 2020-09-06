8198944d8188f64b9e2ee26f25b32ef0eeb9d1b2
dwilliams64 <dwilliams_64@yahoo.com>
2020-09-05 23:00:36 -0500

    Fixed app initial state and  added round function for temp

--- index.js
+++ index.js
@@ -17,53 +17,65 @@ const enable = () => {
     convertTemps.classList.remove("disable-select");    
 }
 
+const roundTemp = temp => String(parseFloat(temp.toFixed(2)));
+
 const convert = (value) => {    
     value = parseFloat(value);
 
     switch (convertTemps.value) {
         case "f-to-c":
             inputLabel.textContent = "Fahrenheit";
-            resultLabel.textContent = "Celsius";            
-            resultTemp.value = parseFloat(((value - 32) / 1.8).toFixed(2));                       
+            resultLabel.textContent = "Celsius";
+            resultTemp.value = roundTemp((value - 32) / 1.8);
             break;
+
         case "c-to-f":
             inputLabel.textContent = "Celsius";
             resultLabel.textContent = "Fahrenheit";            
-            resultTemp.value  = parseFloat(((value * 1.8) + 32).toFixed(2));
+            resultTemp.value = roundTemp((value * 1.8) + 32);
             break;
+
         case "f-to-k":
             inputLabel.textContent = "Fahrenheit";
             resultLabel.textContent = "Kelvin";            
-            resultTemp.value  = parseFloat(((value + 459.67) * (5/9)).toFixed(2));
+            resultTemp.value = roundTemp((value + 459.67) * (5/9));
             break;
+
         case "c-to-k":
             inputLabel.textContent = "Celsius";
             resultLabel.textContent = "Kelvin";                                               
-            resultTemp.value  = parseFloat((value + 273.15).toFixed(2));
+            resultTemp.value = roundTemp(value + 273.15);
             break;
+
         case "k-to-f":
             inputLabel.textContent = "Kelvin";
             resultLabel.textContent = "Fahrenheit";            
-            resultTemp.value  = formatNum(((value * (9/5)) - 459.67).toFixed(2));
+            resultTemp.value = roundTemp((value * (9/5)) - 459.67);
             break;
+
         case "k-to-c":
             inputLabel.textContent = "Kelvin";
             resultLabel.textContent = "Celsius";            
-            resultTemp.value  = formatNum((value - 273.15).toFixed(2));
+            resultTemp.value = roundTemp(value - 273.15);
             break;
+
         defualt:
             console.log("fuck-you")
     }
 }
 
-inputTemp.addEventListener("input", function (e) {
+inputTemp.value = "";
+resultTemp.value = "";
+convertTemps.value = "f-to-c";
+disable();
+
+inputTemp.addEventListener("input", function (e) {    
     if (inputChk(e.target.value)) {
         disable();
     } else {
         enable();
         convert(e.target.value);
     }
-
 });
 
 convertTemps.addEventListener("input", function () {
